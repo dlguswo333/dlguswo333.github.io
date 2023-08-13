c1fe08b38cc1d2ef9e5cb7acebe86e3075a63547
dlguswo333 <dlguswo002@gmail.com>
2023-08-13 15:54:34 +0900

    Setup tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
 <script>
+    import '../app.css';
     let count = 0;
 </script>
-<h1>Welcome to dlguswo333's Blog</h1>
-<button on:click={() => ++count}>{count}</button>
+<h1 class="text-xl hover:text-blue-300">Welcome to dlguswo333's Blog</h1>
+<button class="border border-black p-1 hover:bg-slate-300"
+    on:click={() => ++count}>
+    {count}
+</button>
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import staticAdapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import {vitePreprocess} from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
