91b13ba44354d95ceacd162c2e46cc82e024aa06
dlguswo333 <dlguswo002@gmail.com>
2023-08-13 17:57:47 +0900

    Add Header component

---
+++ src/lib/HeaderLink.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  export let href: string;
+  export let text: string;
+</script>
+<a href={href} class="h-8 flex items-center justify-center mr-2 last:mr-1 px-1 py-0.5 rounded-s hover:bg-gray-200">
+  {text}
+</a>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,14 @@
+<script>
+  import HeaderLink from '$lib/HeaderLink.svelte';
+</script>
+<header class="p-2 flex flex-row align-middle justify-between border-b border-b-gray-300 bg-gray-50">
+  <a href="/" class="inline-block h-8 w-8">
+    <span class='hidden'>Go To Home</span>
+    <img src="/favicon.svg" alt="Logo">
+  </a>
+  <div class="flex flex-row items-center">
+    <HeaderLink href='/categories' text='Categories' />
+    <HeaderLink href='/tags' text='Tags' />
+    <HeaderLink href='/about' text='About' />
+  </div>
+</header>
