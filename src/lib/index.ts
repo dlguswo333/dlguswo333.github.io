c95728407c32037236d4e18426c3c8be90d3d013
dlguswo333 <dlguswo002@gmail.com>
2023-08-14 23:57:25 +0900

    Add Footer component

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
 export const githubUserName = 'dlguswo333';
 export const name = 'dlguswo333';
 export const email = 'dlguswo002@gmail.com';
+export const blogRepositoryLink = 'https://github.com/dlguswo333/dlguswo333.github.io';
---
+++ src/routes/Footer.svelte
@@ -0,0 +1,14 @@
+<script>
+  import {blogRepositoryLink, name} from '$lib';
+</script>
+<footer class="p-2 py-5 text-sm flex gap-1 flex-col md:flex-row items-center justify-between border-t border-b-gray-300 bg-gray-50">
+  <div>
+    ©️ 2021~2023 {name}; All rights reserved.
+  </div>
+  <div class="flex items-center">
+    <a class="inline-flex items-center" href={blogRepositoryLink}>
+      <img class="inline h-4 w-4 mr-1" src="/github-mark.svg" alt="github-mark">
+    </a>
+    Built with Sveltekit.
+  </div>
+</footer>
