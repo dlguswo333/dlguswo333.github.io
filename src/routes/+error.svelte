128ee2b4ec355201633b35ac56f0cbf69dfb0c30
dlguswo333 <dlguswo002@gmail.com>
2023-08-15 00:08:28 +0900

    Edit error page styles

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,7 +1,9 @@
 <script>
   import '../app.css';
 </script>
-<h1 class="text-5xl text-center p-3 mt-5">404</h1>
-<div class="flex justify-center">
-  <a href="/" class="text-2xl underline text-blue-500">Go to Home</a>
+<div>
+  <h1 class="text-5xl text-center p-3 mt-5">404</h1>
+  <div class="flex justify-center">
+    <a href="/" class="text-2xl underline text-blue-500">Go to Home</a>
+  </div>
 </div>
