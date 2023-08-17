3d61d8a27d2a93f63617abab71f24040048b3a29
dlguswo333 <dlguswo002@gmail.com>
2023-08-18 01:49:12 +0900

    Implement basic PostList and PostCard component

---
+++ src/lib/PostCard.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import type crawlPosts from '$lib/crawlPosts';
+
+  export let post: Awaited<ReturnType<typeof crawlPosts>>[0];
+</script>
+
+<section
+  class={'flex flex-col border-2 rounded-md border-gray-30 transition px-4 py-2 hover:border-teal-400 hover:bg-teal-50'}>
+  <div class="flex flex-row items-center justify-between mb-1">
+    <span>
+      <a href={`/post/${post.date.split('-')[0]}/${post.lang}/${post.id}`} class="text-cyan-400 visited:text-blue-600">
+        <h1 class="inline-block font-bold text-lg mr-2 underline">{post.title}</h1>
+      </a>
+    </span>
+    <span class="text-sm">
+      {post.date}
+      {#if post.editedDate}
+        (E. {post.editedDate})
+      {/if}
+    </span>
+  </div>
+  <div class="flex flex-row justify-between">
+    <span>
+      {#if post.category}
+        <span class="bg-sky-300 px-2 py-0.5 rounded-lg shadow-md shadow-emerald-50 border-l-slate-100 text-white">/{post.category}</span>
+      {/if}
+      <span>
+        {#if post.tags}
+          {#each post.tags as tag}
+            <span class="mr-1 first:ml-2 last:mr-0 bg-green-100 px-2 py-0.5 rounded-xl shadow-md">#{tag}</span>
+          {/each}
+        {/if}
+      </span>
+    </span>
+    <span>{post.lang}</span>
+  </div>
+  {#if post.summary}
+    <div class="mt-4">
+      {post.summary}
+    </div>
+  {/if}
+</section>
---
+++ src/lib/PostList.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type crawlPosts from '$lib/crawlPosts';
+  import Postcard from './PostCard.svelte';
+
+  export let posts: Awaited<ReturnType<typeof crawlPosts>>;
+</script>
+<main class="max-w-[1000px]">
+  <ul class="py-4">
+    {#each posts as post}
+      <li class="mb-5 last:mb-0 px-4">
+        <Postcard post={post} />
+      </li>
+    {/each}
+  </ul>
+</main>
