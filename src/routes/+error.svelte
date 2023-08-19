aa019661754402622f03d2cea0d4084b4600d81e
dlguswo333 <dlguswo002@gmail.com>
2023-08-19 20:32:07 +0900

    Edit +error.svelte to show appropriate status code

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,7 +1,8 @@
-<script>
+<script lang="ts">
+  import {page} from '$app/stores';
 </script>
 <div>
-  <h1 class="text-5xl text-center p-3 mt-5">404</h1>
+  <h1 class="text-5xl text-center p-3 mt-5">{$page.status}</h1>
   <div class="flex justify-center">
     <a href="/" class="text-2xl underline text-blue-500">Go to Home</a>
   </div>
