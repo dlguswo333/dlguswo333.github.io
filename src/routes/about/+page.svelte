fa9246f68588280cddc82651c6ac34defc4dbb48
dlguswo333 <dlguswo002@gmail.com>
2023-08-26 20:16:22 +0900

    Implement about page
    I just copied about.md from old repo.
    This because to import files maintaining git histories is not easy.
    And I thought about.md is not worth to maintain histories.

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Profile from '../Profile.svelte';
+
+  export let data;
+</script>
+<div class="flex flex-row flex-grow justify-center p-2 lg:p-0 lg:pr-[300px]">
+  <Profile />
+  <main class="post max-w-[900px] py-2">
+    {@html data.html}
+  </main>
+</div>
