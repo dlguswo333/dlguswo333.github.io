3278872f8fef15bb722360874714df47a0cd2f87
dlguswo333 <dlguswo002@gmail.com>
2023-08-29 21:48:46 +0900

    Implement highlight active heading in TOC

--- src/lib/components/MainSection.svelte
+++ src/lib/components/MainSection.svelte
@@ -1,14 +1,38 @@
 <script lang="ts">
+  import {onMount} from 'svelte';
+  import {activeHeadingIndex} from '$lib/store';
+
   /** Raw html in `string` type */
   export let html: string | null = null;
   /** Additional class to add. */
   export let className: string | null = null;
+
+  let mainHtml: HTMLElement | null = null;
+  onMount(() => {
+    if (!className?.includes('post') || !mainHtml) {
+      return;
+    }
+    const headings = [...mainHtml.querySelectorAll('h1,h2,h3,h4')];
+    const onScroll = () => {
+      headings.some((heading, index) => {
+        // Mark the last heading as active that sits inside the top half of window.
+        if (heading.getBoundingClientRect().top > window.innerHeight / 2) {
+          $activeHeadingIndex = Math.max(Number(index) - 1, 0);
+          return true;
+        } else if (index === headings.length - 1) {
+          $activeHeadingIndex = Number(index);
+        }
+      });
+    };
+    document.addEventListener('scroll', onScroll);
+    return () => document.removeEventListener('scroll', onScroll);
+  });
 </script>
 <!--
 @component
 Render raw html in main section.
 -->
-<main class={`max-w-[900px] w-full py-2 ${className ? className : ''}`}>
+<main class={`max-w-[900px] w-full py-2 ${className ? className : ''}`} bind:this={mainHtml}>
   <slot />
   {#if html}
     {@html html}
--- src/lib/components/TOC.svelte
+++ src/lib/components/TOC.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import type {TOCItem} from '$lib/types';
+  import {activeHeadingIndex} from '$lib/store';
 
   export let data: TOCItem[];
+
   const coefficient = 2 / 3;
   const constant = 0.3;
 </script>
@@ -11,9 +13,9 @@ The scrollable height is not big enough to show the front child 'li' elements.
 -->
 <aside class="w-[300px] overflow-hidden hidden mt-10 self-start md:!flex md:sticky md:top-14 flex-col justify-center items-center p-3">
   <ul class="w-full max-h-[80vh] overflow-auto">
-    {#each data as item}
+    {#each data as item, index}
       <li title={item.text}
-        class={`flex border-l-4 border-transparent hover:border-gray-300 hover:bg-gray-50 ${item.depth === 1 ? 'font-bold' : ''}`}
+        class={`flex border-l-4 border-transparent hover:border-gray-300 hover:bg-gray-50 ${item.depth === 1 ? 'font-bold' : ''} ${$activeHeadingIndex === index ? 'border-blue-500' : ''}`}
         style={`padding-left: ${coefficient * (item.depth - 1) + constant}rem;`}>
         <a href={`#${item.id}`} class="whitespace-nowrap overflow-hidden break-all overflow-ellipsis flex-grow">
           {item.text}
