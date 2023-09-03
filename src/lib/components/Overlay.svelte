ce6e4fe5ad3ccfebba7be69821d05bcedf880f47
dlguswo333 <dlguswo002@gmail.com>
2023-09-03 15:06:05 +0900

    Hide TOC on mousedown Overlay

--- src/lib/components/Overlay.svelte
+++ src/lib/components/Overlay.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
   import {onMount} from 'svelte';
+  import {showTOC} from '$lib/store';
+
+  const hideOverlay = () => {
+    $showTOC = false;
+  };
 
   onMount(() => {
     document.body.style.overflow = 'hidden';
@@ -7,4 +12,5 @@
     return () => {document.body.style.overflow = '';};
   });
 </script>
-<div class="fixed w-full h-full backdrop-blur-lg bg-[#333a] z-10" />
+<div class="fixed w-full h-full backdrop-blur-lg bg-[#333a] z-10"
+  on:mousedown={hideOverlay} role="button" aria-pressed="false" aria-label="Hide TOC" tabindex="0" />
