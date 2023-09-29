95affe26b220e45d5c6904548a1b67c555cfe1af
dlguswo333 <dlguswo002@gmail.com>
2023-09-29 20:05:26 +0900

    [#2] Export get current theme function

--- src/lib/colorTheme.ts
+++ src/lib/colorTheme.ts
@@ -1,7 +1,12 @@
 import type {ColorTheme} from '$lib/types';
 
-const storedValue = JSON.parse(localStorage.getItem('color-theme') || '');
-const systemTheme: ColorTheme = window.matchMedia('(prefers-color-scheme:dark)').matches ? 'dark' : 'light';
-const theme: ColorTheme = storedValue === 'light' || storedValue === 'dark' ? storedValue : systemTheme;
+export const getCurrentTheme = () => {
+  const storedValue = JSON.parse(localStorage.getItem('color-theme') || '');
+  const systemTheme: ColorTheme = window.matchMedia('(prefers-color-scheme:dark)').matches ? 'dark' : 'light';
+  const currentTheme: ColorTheme = storedValue === 'light' || storedValue === 'dark' ? storedValue : systemTheme;
+  return currentTheme;
+};
+
 const rootElement = document.querySelector('div#root');
-theme === 'dark' && rootElement?.classList.add('dark');
+const currentTheme = getCurrentTheme();
+currentTheme === 'dark' && rootElement?.classList.add('dark');
