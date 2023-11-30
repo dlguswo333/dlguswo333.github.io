f4f517bdb0329a0612628a869aececd90401ed55
dlguswo333 <dlguswo002@gmail.com>
2023-11-30 22:44:45 +0900

    [#20] Fix TOC highlight styles

--- src/lib/components/TOC.svelte
+++ src/lib/components/TOC.svelte
@@ -36,22 +36,23 @@ The scrollable height is not big enough to show the front child 'li' elements.
 -->
 <aside class={`w-[300px] overflow-hidden hidden mt-10 self-start md:!flex md:sticky md:top-14 flex-col justify-center items-center p-3
   ${$showTOC ? '!w-[94vw] py-6 z-20 !flex fixed top-14 left-[50%] translate-x-[-50%] border-2 border-neutral-300 bg-neutral-50 shadow-lg rounded-md dark:bg-neutral-800' : ''}`}>
-  <ul class="relative w-full max-h-[80vh] overflow-auto">
-    {#each data as item}
-      <li title={item.text}
-        class={`flex border-l-4 border-transparent hover:border-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 ${item.depth === 1 ? 'font-bold' : ''} ${$showTOC ? 'py-1.5' : ''}
+  <div class="relative w-full">
+    <ul class="max-h-[80vh] overflow-auto">
+      {#each data as item}
+        <li title={item.text}
+          class={`flex border-l-4 border-transparent hover:border-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 ${item.depth === 1 ? 'font-bold' : ''} ${$showTOC ? 'py-1.5' : ''}
         `}
-      >
-        <a href={`#${item.id}`} class="py-0.5 whitespace-nowrap overflow-hidden break-all overflow-ellipsis flex-grow"
-          style={`padding-left: ${coefficient * (item.depth - 1) + constant}rem;`}>
-          {item.text}
-        </a>
-      </li>
-    {/each}
-  </ul>
-  <!-- [TODO] Need to improve styles -->
-  {#if highlightTop !== undefined && highlightBottom !== undefined}
-    <!-- [TODO] mt-3 because there is p-3 in the parent container. Need more explicit way to handle this. -->
-    <div class={'absolute left-0 bg-blue-400 w-1 mt-3'} style={`top: ${highlightTop}px; height:${highlightBottom - highlightTop}px`} />
-  {/if}
+        >
+          <a href={`#${item.id}`} class="py-0.5 whitespace-nowrap overflow-hidden break-all overflow-ellipsis flex-grow"
+            style={`padding-left: ${coefficient * (item.depth - 1) + constant}rem;`}>
+            {item.text}
+          </a>
+        </li>
+      {/each}
+    </ul>
+    <!-- [TODO] Need to improve styles -->
+    {#if highlightTop !== undefined && highlightBottom !== undefined}
+      <div class={'absolute left-0 bg-blue-400 w-1'} style={`top: ${highlightTop}px; height:${highlightBottom - highlightTop}px`} />
+    {/if}
+  </div>
 </aside>
