79f992410d952e9b05b187d1a56a411bd72d4304
dlguswo333 <dlguswo002@gmail.com>
2024-05-22 23:11:01 +0900

    Format svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,12 +3,12 @@ import {vitePreprocess} from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: staticAdapter({
+  kit: {
+    adapter: staticAdapter({
       // default options are shown. On some platforms
       // these options are set automatically — see below
       pages: 'build',
@@ -21,7 +21,9 @@ const config = {
       crawl: true,
       entries: ['*'],
     }
-	}
+  }
 };
 
 export default config;
+
+
