a845d4d9cf2fc8a7601fb528f8ec51f4df23c797
dlguswo333 <dlguswo002@gmail.com>
2024-11-16 20:26:27 +0900

    Edit HeaderLink styles

--- src/lib/components/HeaderLink.svelte
+++ src/lib/components/HeaderLink.svelte
@@ -4,9 +4,9 @@
   export let isActive: boolean;
 </script>
 <a href={href} class={
-  `h-8 flex items-center justify-center mr-2 last:mr-1 px-1 py-0.5 rounded active:bg-cyan-50
+  `h-8 flex items-center justify-center mr-2 last:mr-1 px-1 py-0.5 rounded dark:active:bg-cyan-900 active:bg-cyan-50
    [@media(hover:hover)]:[@media(pointer:fine)]:hover:bg-gray-200 [@media(hover:hover)]:[@media(pointer:fine)]:dark:hover:bg-slate-600
    [@media(hover:hover)]:[@media(pointer:fine)]:dark:hover:text-white
-   ${isActive ? 'bg-sky-100 dark:bg-cyan-900' : ''}`}>
+   ${isActive ? 'bg-sky-100 dark:bg-sky-900' : ''}`}>
   {text}
 </a>
