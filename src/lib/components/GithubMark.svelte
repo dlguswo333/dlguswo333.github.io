caf318913f9fdb594db499d78344c4b202047810
dlguswo333 <dlguswo002@gmail.com>
2024-12-28 14:28:50 +0900

    Fix Github icon not matching themes

--- src/lib/components/GithubMark.svelte
+++ src/lib/components/GithubMark.svelte
@@ -1,12 +1,5 @@
 <script lang="ts">
-  import {currentTheme as themeStore} from '$lib/store';
-
   export let className: string | null = null;
-
-  const getGithubImgSrc = (currentTheme: typeof $themeStore) => {
-    return currentTheme === 'dark' ? '/github-mark-white.svg' : '/github-mark.svg';
-  };
-  let githubImgSrc = getGithubImgSrc($themeStore);
-  $: githubImgSrc = getGithubImgSrc($themeStore);
 </script>
-<img class={'inline h-4 w-4' + (className ? (' ' + className) : '')} src={githubImgSrc} alt="github-mark">
+<img class={'inline dark:hidden h-4 w-4' + (className ? (' ' + className) : '')} src='/github-mark.svg' alt="github-mark">
+<img class={'hidden dark:inline h-4 w-4' + (className ? (' ' + className) : '')} src='/github-mark-white.svg' alt="github-mark">
--- src/lib/store.ts
+++ src/lib/store.ts
@@ -12,4 +12,5 @@ export const shouldShowTOCButton = writable(false);
 
 export const shouldShowOverlay = derived(showTOC, ($showTOC) => $showTOC);
 
+/** @deprecated Not used anywhere but left for unknown future. */
 export const currentTheme = writable<ColorTheme | null>(null);
