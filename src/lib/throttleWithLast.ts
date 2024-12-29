271bb5141bbc0bd0ed9cf168d36e6d2493a7cdf1
dlguswo333 <dlguswo002@gmail.com>
2024-12-29 13:36:54 +0900

    Refactor throttleWithLast function
    Make it a generic function.
    Do not call the wrapped function immediately.
    Change logics.

--- src/lib/throttleWithLast.ts
+++ src/lib/throttleWithLast.ts
@@ -1,17 +1,22 @@
-const throttleWithLast = (func: () => unknown, interval: number) => {
+const throttleWithLast = <T extends unknown[]>(func: (...args: T) => unknown, interval: number) => {
   let lastTimestamp = 0;
-  let curTimeStamp = 0;
-  let timer: ReturnType<typeof setTimeout> | number = 0;
+  let lastTimer: ReturnType<typeof setTimeout> | number = 0;
 
-  return function () {
-    curTimeStamp = Date.now();
-    clearTimeout(timer);
-    if (curTimeStamp - lastTimestamp >= interval) {
-      lastTimestamp = curTimeStamp;
-      func();
-    } else {
-      timer = setTimeout(func, interval);
+  return (...args: T) => {
+    const curTimeStamp = Date.now();
+    const curTimer = setTimeout(() => {
+      func(...args);
+    }, interval);
+    if (curTimeStamp - lastTimestamp <= interval) {
+      // It might be the last call.
+      // Cancel the last timer which was expected to be the last call,
+      // and register it as the last call.
+      clearTimeout(lastTimer);
+      lastTimer = curTimer;
+      return;
     }
+    // Refresh the last timestamp on the first call within each interval only.
+    lastTimestamp = curTimeStamp;
   };
 };
 
