6f3f5e3eeab4381c33a080ded973ef38c189b23c
dlguswo333 <dlguswo002@gmail.com>
2025-01-17 23:18:36 +0900

    Fix Tag style: shadow clipped

--- src/lib/components/Tag.svelte
+++ src/lib/components/Tag.svelte
@@ -10,8 +10,14 @@
   const onClick = () => goto(`/tags#${tagName}`);
 </script>
 <button onclick={onClick}
-  class="last:mr-0 bg-green-100 hover:bg-green-400 hover:text-white dark:text-black transition px-2 pl-3 py-0.5 rounded-sm shadow-md shadow-emerald-100 dark:shadow-green-900 my-0.5"
-  style={`margin-right: ${marginRight * 0.25}rem; margin-left: ${marginLeft * 0.25}rem; clip-path: polygon(8px 0, 0 50%, 8px 100%, 100% 100%, 100% 0);`}
+  class="my-0.5 last:mr-0 bg-transparent rounded-sm overflow-hidden"
+  style:margin-left={`${marginLeft * 0.25}rem`}
+  style:margin-right={`${marginRight * 0.25}rem`}
+  style:filter="drop-shadow(0px 2px 3px rgb(100, 140, 120))"
 >
-  #{tagName}
+  <div class="bg-green-100 hover:bg-green-400 hover:text-white dark:text-black transition px-2 pl-3 py-0.5"
+    style:clip-path="polygon(8px 0, 0 50%, 8px 100%, 100% 100%, 100% 0)"
+  >
+    #{tagName}
+  </div>
 </button>
