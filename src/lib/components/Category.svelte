5aa592aa3b375738cece783103cb8eac00771078
dlguswo333 <dlguswo002@gmail.com>
2025-01-19 10:00:19 +0900

    Change Category style

--- src/lib/components/Category.svelte
+++ src/lib/components/Category.svelte
@@ -8,7 +8,7 @@
   const onClick = () => goto(`/categories#${categoryName}`);
 </script>
 <button onclick={onClick}
-  class="px-2 py-0.5 rounded-lg shadow-md shadow-emerald-50 border-l-slate-100
+  class="px-2 py-0.5 rounded-lg shadow-md shadow-emerald-800/30 border-l-slate-100
     bg-sky-300 text-white hover:bg-sky-500 dark:shadow-emerald-900 transition"
 >
   /{categoryName}
