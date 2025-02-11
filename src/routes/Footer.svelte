0998d838ad55a89498dde72b705da892ce2442f6
dlguswo333 <dlguswo002@gmail.com>
2025-02-11 22:37:54 +0900

    Sync end year to build date in Footer component

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   import {blogRepositoryLink, name} from '$lib';
   import GithubMark from '$lib/components/GithubMark.svelte';
+  const startYear = 2021;
+  const endYear = new Date().getFullYear();
 </script>
 <footer class="p-2 py-5 text-sm flex gap-1 flex-col md:flex-row items-center justify-between border-t border-gray-300 bg-gray-50 dark:border-gray-700 dark:bg-[#2c3039]">
   <div>
-    © 2021~2024 {name}; All rights reserved.
+    © {startYear}-{endYear} {name}; All rights reserved.
   </div>
   <div class="flex items-center">
     <a class="inline-flex items-center" href={blogRepositoryLink}>
