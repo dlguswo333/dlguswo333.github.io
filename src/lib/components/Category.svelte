7f3a6342c26924967e626384b5d4a16b32d3c5a0
dlguswo333 <dlguswo002@gmail.com>
2025-02-17 21:18:55 +0900

    Add type attribute to button element in Category component

--- src/lib/components/Category.svelte
+++ src/lib/components/Category.svelte
@@ -7,7 +7,9 @@
   let {categoryName}: Props = $props();
   const onClick = () => goto(`/categories#${categoryName}`);
 </script>
-<button onclick={onClick}
+<button
+  onclick={onClick}
+  type="button"
   class="px-2 py-0.5 rounded-lg shadow-md shadow-emerald-800/30 border-l-slate-100
     bg-sky-300 text-white hover:bg-sky-500 dark:shadow-emerald-900 transition"
 >
