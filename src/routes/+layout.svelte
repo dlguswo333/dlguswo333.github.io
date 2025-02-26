99cdfc2b25c6ae195f963edbc789a61b0cc392c8
dlguswo333 <dlguswo002@gmail.com>
2025-02-26 20:23:26 +0900

    Move title element from html to root layout
    This also fixes duplicated title elements in rendered html.
    'contents of <svelte:head> are returned separately from the rest of your HTML.'

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,12 +4,16 @@
   import Header from './Header.svelte';
   import Overlay from '$lib/components/Overlay.svelte';
   import {shouldShowOverlay} from '$lib/store';
+  import {name} from '$lib/index';
   interface Props {
     children?: import('svelte').Snippet;
   }
 
   let {children}: Props = $props();
 </script>
+<svelte:head>
+  <title>{name}'s blog</title>
+</svelte:head>
 <Header />
 {@render children?.()}
 <Footer />
