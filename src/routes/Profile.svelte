5c8e79a87a0283b86df94f70b4b0030db84e4ede
dlguswo333 <dlguswo002@gmail.com>
2025-02-26 20:05:38 +0900

    Fix html spec error: for attribute of label must be the id of a form control

--- src/routes/Profile.svelte
+++ src/routes/Profile.svelte
@@ -6,17 +6,17 @@
 </script>
 <aside class="max-w-[300px] hidden mt-10 self-start md:!flex flex-col justify-center items-center p-5">
   <img class="mb-5 rounded-xl border-2 border-slate-200" src={profileImageSrc} alt="profile" width="110" height="110">
-  <ul class="[&>li]:p-0.5 [&_label]:inline-flex [&_label]:items-center [&>li:not(:last-child)]:border-b [&>li:not(:last-child)]:border-b-gray-300 [&>li:not(:last-child)]:pb-1 [&>li:not(:first-child)]:pt-1 [&>li:not(:first-child)]:text-sm whitespace-nowrap">
+  <ul class="[&>li]:p-0.5 [&_span]:inline-flex [&_span]:items-center [&>li:not(:last-child)]:border-b [&>li:not(:last-child)]:border-b-gray-300 [&>li:not(:last-child)]:pb-1 [&>li:not(:first-child)]:pt-1 [&>li:not(:first-child)]:text-sm whitespace-nowrap">
     <li class="flex justify-center">
       <span>{name}</span>
     </li>
     <li>
-      <label for="profile-email">@:</label>
-      <a href={`mailto:${email}`} id="profile-email" class="text-blue-500 hover:underline">{email}</a>
+      <span>@:</span>
+      <a href={`mailto:${email}`} class="text-blue-500 hover:underline">{email}</a>
     </li>
     <li>
-      <label for="profile-github"><GithubMark />:</label>
-      <a href={`https://github.com/${githubUserName}`} id="profile-github" class="text-blue-500 hover:underline">Github Link</a>
+      <span><GithubMark />:</span>
+      <a href={`https://github.com/${githubUserName}`} class="text-blue-500 hover:underline">Github Link</a>
     </li>
   </ul>
 </aside>
