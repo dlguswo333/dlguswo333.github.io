73f03ee2dafb108eace1373805cbd20b9ef70702
dlguswo333 <dlguswo002@gmail.com>
2026-02-07 17:01:18 +0900

    Add interaction to copy button success

--- src/routes/CodeBlock.svelte
+++ src/routes/CodeBlock.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+  import {getClasses} from '$lib/string';
+
   interface Props {
     children?: import('svelte').Snippet;
     preProperties?: Record<string, unknown>;
   }
   let preElement: HTMLPreElement;
   let {children, preProperties}: Props = $props();
+  let copyResult = $state<boolean | null>(null);
 
   const getLanguage = () => {
     if (typeof preProperties?.class !== 'string') {
@@ -19,11 +22,18 @@
   const language = getLanguage();
 
   const copy = () => {
+    if (copyResult) {
+      return;
+    }
     try {
       if (!preElement.textContent) {
         throw new Error('Text does not exist');
       }
       navigator.clipboard.writeText(preElement.textContent);
+      copyResult = true;
+      setTimeout(() => {
+        copyResult = false;
+      }, 1000);
     } catch {
       alert('Writing to clipboard failed. Please check if your browser supports clipboard.');
     }
@@ -36,7 +46,15 @@
         {language}
       {/if}
     </div>
-    <button onclick={copy} class="px-2 py-1 rounded-md text-sm bg-slate-100 dark:bg-slate-700 text-black dark:text-white hover:bg-slate-200 dark:hover:bg-slate-500 active:bg-emerald-300 dark:active:bg-emerald-400">Copy</button>
+    <button onclick={copy} class={getClasses('relative px-2 py-1 rounded-md text-sm bg-slate-100 dark:bg-slate-700 text-black dark:text-white hover:bg-slate-200 dark:hover:bg-slate-500 active:bg-emerald-300 dark:active:bg-emerald-400', {'text-transparent': !!copyResult})}>
+      Copy
+      {#if copyResult}
+        <svg xmlns="http://www.w3.org/2000/svg" class="absolute w-[100%] h-[100%] left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] aspect-square p-1" viewBox="0 0 100 100">
+          <circle cx="50" cy="50" r="50" fill="#3d3" />
+          <path d="M 20 50 L 40 70 L 80 30 M 20 50 Z" stroke="white" stroke-linecap="round" stroke-width="10" fill="none" />
+        </svg>
+      {/if}
+    </button>
   </div>
   <!-- [NOTE] whitespaces within pre tag are render as-is even the indentations within the tag. -->
   <pre {...preProperties} bind:this={preElement}>{@render children?.()}</pre>
