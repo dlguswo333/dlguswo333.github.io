fd4ea49c19fda320a59b237bde6b6795194da492
dlguswo333 <dlguswo002@gmail.com>
2026-02-08 12:53:10 +0900

    Rename variable

--- src/routes/CodeBlock.svelte
+++ src/routes/CodeBlock.svelte
@@ -7,7 +7,7 @@
   }
   let preElement: HTMLPreElement;
   let {children, preProperties}: Props = $props();
-  let copyResult = $state<boolean | null>(null);
+  let copySuccess = $state<boolean | null>(null);
 
   const getLanguage = () => {
     if (typeof preProperties?.class !== 'string') {
@@ -22,7 +22,7 @@
   const language = getLanguage();
 
   const copy = () => {
-    if (copyResult) {
+    if (copySuccess) {
       return;
     }
     try {
@@ -30,9 +30,9 @@
         throw new Error('Text does not exist');
       }
       navigator.clipboard.writeText(preElement.textContent);
-      copyResult = true;
+      copySuccess = true;
       setTimeout(() => {
-        copyResult = false;
+        copySuccess = null;
       }, 1000);
     } catch {
       alert('Writing to clipboard failed. Please check if your browser supports clipboard.');
@@ -47,10 +47,10 @@
       {/if}
     </div>
     <div class="relative">
-      <button onclick={copy} class={getClasses('px-2 py-1 rounded-md text-sm bg-slate-100 dark:bg-slate-700 text-black dark:text-white hover:bg-slate-200 dark:hover:bg-slate-500 active:bg-emerald-300 dark:active:bg-emerald-400', {'invisible': !!copyResult})}>
+      <button onclick={copy} class={getClasses('px-2 py-1 rounded-md text-sm bg-slate-100 dark:bg-slate-700 text-black dark:text-white hover:bg-slate-200 dark:hover:bg-slate-500 active:bg-emerald-300 dark:active:bg-emerald-400', {'invisible': !!copySuccess})}>
         Copy
       </button>
-      {#if copyResult}
+      {#if copySuccess}
         <svg xmlns="http://www.w3.org/2000/svg" class="overflow-visible absolute w-[100%] h-[100%] left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] aspect-square p-1" viewBox="0 0 100 100">
           <circle cx="50" cy="50" r="50" fill="#3d3" />
           <path d="M 20 50 L 40 70 L 80 30 M 20 50 Z" stroke="white" stroke-linecap="round" stroke-width="10" fill="none" />
